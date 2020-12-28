346f9ed566a538cf218ef95f3b1962c6ab7db1cb
Bassel Al Omari <baselomari@hotmail.com>
2020-12-29 00:30:08 +0200

    Perfect Data Entry and Organized Data into appropriate list

--- Day 7 Part 1.py
+++ Day 7 Part 1.py
@@ -5,14 +5,17 @@ data = [line.strip() for line in data]
 
 color_rules = []
 
+count = 0
 for rule in data:
     rule_string = rule.split()
     contained_bags = []
 
-    for i, text in enumerate(rule_string[4:]):
-        if text == "bag." or text == "bags." or text == "bag," or text == "bags,":
-            contained_bags.append([int(rule_string[i-3]),rule_string[i-2] + " " + rule_string[i-1]])
+    if rule[-22:] != "contain no other bags.":
+        for i, text in enumerate(rule_string[4:]):
+            x = i+4
+            if text == "bag." or text == "bags." or text == "bag," or text == "bags,":
+                contained_bags.append([int(rule_string[x-3]),rule_string[x-2] + " " + rule_string[x-1]])
         
     color_rules.append([rule_string[0] + " " + rule_string[1], contained_bags])
 
-print(color_rules)
+    count += 1
