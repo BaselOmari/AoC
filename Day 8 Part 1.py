925014d8a9b3136364d9c1c0fe742a161f42bd6c
Bassel Al Omari <baselomari@hotmail.com>
2020-12-30 02:37:22 +0200

    Day 8 Done

--- Day 8 Part 1.py
+++ Day 8 Part 1.py
@@ -1,6 +1,33 @@
-with open("Day8Data.txt", 'r') as f:
+with open("DATA/Day8Data.txt", 'r') as f:
     data = f.readlines()
 
 data = [line.strip() for line in data]
 
-print(data)
+
+data = [[line[:3], int(line[4:])] for line in data]
+
+visited_index = []
+acc_val = 0
+
+def in_visited(index):
+    for i in visited_index:
+        if i == index:
+            return True
+    
+    return False
+
+i = 0
+while True:
+    ting = data[i]
+    visited_index.append(i)
+    if ting[0] == 'acc':
+        acc_val += ting[1]
+    elif ting[0] == 'jmp':
+        i += ting[1] - 1
+    i += 1
+
+    if in_visited(i):
+        print(acc_val)
+        break
+
+    
